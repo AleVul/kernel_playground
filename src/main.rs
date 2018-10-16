aa608626764e97e96ddfb1ef9007468254c78177
Alexandru Vulpea <alexandru.vulpea@gmail.com>
2018-10-16 21:12:04 +0300

    Build a freestandig binary
    build freestand binary;
    add comments;
    add .gitignore file.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+// std is OS specific so it does not work here.
+#![no_std]
+
+// To avoid `crt0` and `start` language item.
+// Since we dont run over an OS but implement one.
+#![no_main]
+
+use core::panic::PanicInfo;
+
+/// On linux, the linker looks for a function
+/// named `_start` to use as an entry point. So
+/// this is what we will use.
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
+/// Panic handler, will be called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
