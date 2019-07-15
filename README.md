b3533b7dfcf17348ea8213a7e11e2889d5dcb5fe
Alexandr Vulpea <alexandru.vulpea@gmail.com>
2019-07-15 21:34:01 +0300

    add vga_buffer update deps

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Kernel playground
 Playground for kernel development and experiments mostly written following the excelent tutorials from https://os.phil-opp.com/
+
+## Running
+To run the kernel, you need to install `bootimage` version >= 0.7.3.
+
+Then just run `cargo xrun`.
---
+++ src/vga_buffer.rs
@@ -0,0 +1,149 @@
+use volatile::Volatile;
+use lazy_static::lazy_static;
+use core::fmt;
+
+const BUFFER_HEIGHT: usize = 25;
+const BUFFER_WIDTH: usize = 80;
+static NEW_LINE_BYTE = b"/n";
+
+#[allow(dead_code)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u8)]
+pub enum Color {
+    Black = 0,
+    Blue = 1,
+    Green = 2,
+    Cyan = 3,
+    Red = 4,
+    Magenta = 5,
+    Brown = 6,
+    LightGray = 7,
+    DarkGray = 8,
+    LightBlue = 9,
+    LightGreen = 10,
+    LightCyan = 11,
+    LightRed = 12,
+    Pink = 13,
+    Yellow = 14,
+    White = 15,
+}
+
+/// This struct respresents background/foreground color
+/// for the text in the VGA buffer (it just wraps an `u8`).
+/// 
+/// Thus we use `repr(transparent)` so that it has the same ABI as
+/// an u8 wi9th the added benefits of being a struct that can have methods.
+/// 
+/// see: https://doc.rust-lang.org/nomicon/other-reprs.html#reprtransparent
+/// 
+/// # Examples
+/// 
+/// ```
+/// use vga_buffer;
+/// 
+/// let color = ColorCode::new(Color::Black, Color::White);
+/// ```
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(transparent)]
+struct ColorCode(u8);
+
+impl ColorCode {
+    /// Creates a new instance of `ColorCode`.
+    fn new(foreground: Color, background: Color) -> ColorCode {
+        // shift background color 4 bits to the left.
+        ColorCode((background as u8) << 4 | (foreground as u8))
+    }
+}
+
+impl Default for ColorCode {
+    fn default() -> Self { ColorCode((Color::Black as u8) << 4 | (Color::White as u8)) }
+}
+
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(C)]
+struct ScreenChar {
+    ascii_character: u8,
+    color_code: ColorCode,
+}
+
+#[repr(transparent)]
+struct Buffer {
+    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
+}
+
+pub struct Writer {
+    column_position: usize,
+    color_code: ColorCode,
+    buffer: &'static mut Buffer,
+}
+
+impl Writer {
+    pub fn write_byte(&mut self, byte: u8) {
+        match byte {
+            NEW_LINE_BYTE => self.new_line(),
+            byte => {
+                if self.column_position >= BUFFER_WIDTH {
+                    self.new_line();
+                }
+
+                let row = BUFFER_HEIGHT - 1;
+                let col = self.column_position;
+
+                let color_code = self.color_code;
+                self.buffer.chars[row][col].write(ScreenChar {
+                    ascii_character: byte,
+                    color_code,
+                });
+                self.column_position += 1;
+            }
+        }
+    }
+
+    pub fn write_string(&mut self, s: &str) {
+        for byte in s.bytes() {
+            match byte {
+                // printable ASCII byte or newline
+                0x20..=0x7e | b'\n' => self.write_byte(byte),
+                // not part of printable ASCII range
+                _ => self.write_byte(0xfe),
+            }
+        }
+    }
+
+    fn new_line(&mut self) {
+         for row in 1..BUFFER_HEIGHT {
+            for col in 0..BUFFER_WIDTH {
+                let character = self.buffer.chars[row][col].read();
+                self.buffer.chars[row - 1][col].write(character);
+            }
+        }
+        self.clear_row(BUFFER_HEIGHT - 1);
+        self.column_position = 0;
+    }
+
+    fn clear_row(&mut self, row: usize) {
+        let blank = ScreenChar {
+            ascii_character: b' ',
+            color_code: self.color_code,
+        };
+        for col in 0..BUFFER_WIDTH {
+            self.buffer.chars[row][col].write(blank);
+        }
+    }
+}
+
+impl fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.write_string(s);
+        Ok(())
+    }
+}
+
+lazy_static! {
+    pub static ref WRITER: Writer = Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::Yellow, Color::Black),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    };
+}
