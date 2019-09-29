71b2a0a9da90b9996270ff15a5ee5812ccfb1960
Alexandr Vulpea <alexandru.vulpea@gmail.com>
2019-09-29 11:36:31 +0300

    Finish testing module
    - add lib module in kernel
    - add basic integration tests
    - add serial port print

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@
 #![no_main]
 // for custom testing framework
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(kernel_playground::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-mod vga_buffer;
+use kernel_playground::println;
 
 /// On linux, the linker looks for a function
 /// named `_start` to use as an entry point. So
@@ -18,6 +18,7 @@ mod vga_buffer;
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
+    // For unit tests
     #[cfg(test)]
     test_main();
 
@@ -25,40 +26,16 @@ pub extern "C" fn _start() -> ! {
 }
 
 /// Panic handler, will be called on panic.
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     println!("{}", _info);
     loop {}
 }
 
+// Panic handler in test mode
 #[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-    }
-    exit_qemu(QemuExitCode::Success);
-}
-
-#[test_case]
-fn trivial_assertion() {
-    print!("trivial assertion... ");
-    assert_eq!(1, 1);
-    println!("[ok]");
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    kernel_playground::test_panic_handler(info)
 }
